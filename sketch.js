0c102d412df819a37361cdc15fd30ece7f409ae4
Caleb Harmon <calebmharmondev@gmail.com>
2019-07-27 15:51:06 -0500

    changed to robot

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var img;
+var imgClone;
+var mk;
+ 
+function setup() {
+    createCanvas(400, 400);
+	xSlider = createSlider(0,400,200);
+}
+function draw() {
+    background(200);
+	line(xSlider.value(),0,xSlider.value(),height);
+}
