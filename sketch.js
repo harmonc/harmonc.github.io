4baa8c5cccd1aaaa12f01a5f8a419dc5a62140ce
Caleb Harmon <calebmharmondev@gmail.com>
2019-07-27 16:12:58 -0500

    fixed log

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
 	xSlider = createSlider(0,400,200);
 }
 function draw() {
-	xSlider.value().log();
+	console.log(xSlider.value());
     background(200);
 	line(xSlider.value(),0,xSlider.value(),height);
 }
