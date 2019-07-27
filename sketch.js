fad176d98a3c96f2f537023f1912246116696b93
Caleb Harmon <calebmharmondev@gmail.com>
2019-07-27 16:34:07 -0500

    added input

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ var mk;
  
 function setup() {
     createCanvas(400, 400);
+	var inp = createInput('');
+	inp.input(myInputEvent);
     xSlider = createSlider(0,400,200);
 }
 function draw() {
@@ -11,4 +13,8 @@ function draw() {
     background(255);
 	line(xSlider.value(),0,xSlider.value(),height);
 	rect(xSlider.value(),height/2,50,50);
+}
+
+function myInputEvent(){
+	console.log('you are typing: ',this.value);
 }
