d72a742581fea06b1e4684cf5d037eb17f21ca78
Caleb Harmon <calebmharmon@gmail.com>
2020-02-17 18:40:39 -0600

    added more squids

--- sketches/squids/sketch.js
+++ sketches/squids/sketch.js
@@ -1,19 +1,27 @@
-var squid;
+var squids;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  squid = new Squid(window.innerWidth/2,window.innerHeight/2,HALF_PI,1, random(100000));
+  squids = [];
+  for (var i = 0; i < 5; i++) {
+    squids.push(new Squid(random(window.innerWidth), random(window.innerHeight), HALF_PI, 1, random(100000)));
+  }
 }
 
 function draw() {
-  background(15);
+  background(0, 105, 148);
   stroke(255);
-  point(mouseX,mouseY);
-  squid.display();
+  point(mouseX, mouseY);
+  for (var i = 0; i < squids.length; i++) {
+    squids[i].display();
+  }
 }
 
-function mousePressed(){
-  squid = new Squid(window.innerWidth/2,window.innerHeight/2,HALF_PI,1, random(100000));
+function mousePressed() {
+  squids = [];
+  for (var i = 0; i < 5; i++) {
+    squids.push(new Squid(random(window.innerWidth), random(window.innerHeight), HALF_PI, 1, random(100000)));
+  }
 }
 
 function windowResized() {
