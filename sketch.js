096f1008635d15bb717d02eecbc5357024854842
Caleb Harmon <calebmharmon@gmail.com>
2020-02-26 16:19:20 -0600

    new background

--- sketch.js
+++ sketch.js
@@ -1,39 +1,75 @@
 var canvas;
-var arr = [];
-var noiseArr = [];
-var sideLength = 50;
+var size;
+var w;
+var h;
+var grid;
+var snake;
 
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight);
   canvas.position(0, 0);
   canvas.style('z-index', -1);
-  var num = height/sideLength;
-  for(var i = 0; i < num; i++){
-    arr.push(width/2);
-    noiseArr.push(random(100));
+  size = 10;
+  w = int(window.innerWidth / size) + 1;
+  h = int(window.innerHeight / size) + 1;
+  grid = [];
+  for (var i = 0; i < h; i++) {
+    var row = [];
+    for (var j = 0; j < w; j++) {
+      row.push(false);
+    }
+    console.log(row);
+    grid.push(row);
   }
+  console.log(grid);
+  snake = new Snake(int(random(grid[0].length)), int(random(grid.length)));
+  console.log(snake.xPos);
+  console.log(snake.yPos);
+  grid[snake.yPos][snake.xPos] = true;
+  background(12);
 }
 
 function draw() {
-  background(15);
-  for(var i = 0; i < height; i+=sideLength){
-    stroke(255,5);
-    line(0,i,width,i);
 
-    if(mouseY<=i+sideLength&&mouseY>i){
-      arr[i/sideLength]=mouseX-sideLength/2;
-    }else{
-      arr[i/sideLength]+=map(noise(noiseArr[i/sideLength]),0,1,-2,2);
-      arr[i/sideLength]%=width;
-      if(arr[i/sideLength]<-sideLength){
-        arr[i/sideLength]=width-sideLength;
-      }
-      noiseArr[i/sideLength]+=.001;
+  var moved = snake.move(grid);
+  if (!moved) {
+    snake = respawn();
+  }
+}
+
+function respawn() {
+  var result = null;
+  var totalSpots = grid[0].length * grid.length;
+  var startPos = int(random(totalSpots));
+  var pos = startPos;
+  var xPos = int(pos % grid[0].length);
+  var yPos = int(pos / grid[0].length);
+
+  var done = false;
+  while (!validPostion(xPos, yPos) && !done) {
+    pos = (pos + 1) % totalSpots;
+    xPos = int(pos % grid[0].length);
+    yPos = int(pos / grid[0].length);
+    if (startPos==pos){
+      done = true;
     }
+  }
+  if(!done){
+    grid[yPos][xPos] = true;
+    result = new Snake(xPos,yPos);
+  }
+  return result;
+}
 
-    noStroke();
-    fill(0,0,255,50);
-    rect(arr[i/sideLength],i,sideLength,sideLength);
+function validPostion(x, y){
+  return !gridValue(x,y) && !gridValue(x+1,y) && !gridValue(x-1,y) && !gridValue(x,y+1) && !gridValue(x,y-1);
+}
+
+function gridValue(x, y){
+  if(x >= 0 && y >= 0 && x < grid[0].length && y < grid.length){
+    return grid[y][x];
+  }else{
+    return false;
   }
 }
 
---
+++ snake.js
@@ -0,0 +1,76 @@
+class Snake {
+
+  constructor(x, y) {
+    this.xPos = x;
+    this.yPos = y;
+    this.dir = int(random(4));
+  }
+
+  inBoundaries(grid, x, y) {
+    return x >= 0 && y >= 0 && x < grid[0].length && y < grid.length;
+  }
+
+  isSnake(grid, x, y) {
+    var result = false;
+    if (this.inBoundaries(grid, x, y)) {
+      result = grid[y][x];
+    }
+    return result;
+  }
+
+  move() {
+
+    if (this.inBoundaries(grid, this.xPos, this.yPos)) {
+      grid[this.yPos][this.xPos] = true;
+    }
+
+    var moved = false;
+    var count = 0;
+    while (!moved && count < 4) {
+      if (random(1) > .8) {
+        this.dir = int(random(4));
+      }
+      switch (this.dir) {
+        case 0:
+          if (this.inBoundaries(grid, this.xPos + 1, this.yPos) && !this.isSnake(grid, this.xPos + 2, this.yPos) && !this.isSnake(grid, this.xPos + 1, this.yPos + 1) && !this.isSnake(grid, this.xPos + 1, this.yPos - 1)) {
+            this.xPos += 1;
+            moved = true;
+          }
+          break;
+        case 1:
+          if (this.inBoundaries(grid, this.xPos, this.yPos - 1) && !this.isSnake(grid, this.xPos, this.yPos - 2) && !this.isSnake(grid, this.xPos + 1, this.yPos - 1) && !this.isSnake(grid, this.xPos - 1, this.yPos - 1)) {
+            this.yPos -= 1;
+            moved = true;
+          }
+          break;
+        case 2:
+          if (this.inBoundaries(grid, this.xPos - 1, this.yPos) && !this.isSnake(grid, this.xPos - 2, this.yPos) && !this.isSnake(grid, this.xPos - 1, this.yPos + 1) && !this.isSnake(grid, this.xPos - 1, this.yPos - 1)) {
+            this.xPos -= 1;
+            moved = true;
+          }
+          break;
+        case 3:
+          if (this.inBoundaries(grid, this.xPos, this.yPos + 1) && !this.isSnake(grid, this.xPos, this.yPos + 2) && !this.isSnake(grid, this.xPos + 1, this.yPos + 1) && !this.isSnake(grid, this.xPos - 1, this.yPos + 1)) {
+            this.yPos += 1;
+            moved = true;
+          }
+          break;
+      }
+
+      if (!moved) {
+        this.dir = (this.dir + 1) % 4;
+      }
+
+      count++;
+    }
+    if (moved) {
+      fill(0, 0, 50);
+      noStroke();
+      rect(snake.xPos * size, snake.yPos * size, size, size);
+    }
+    if (this.inBoundaries(grid, this.xPos, this.yPos)) {
+      grid[this.yPos][this.xPos] = true;
+    }
+    return moved;
+  }
+}
